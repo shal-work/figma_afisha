6b8d8f727e187a6120d31b0cac6a098be837625d
Alex ШАЛ <shal.work@mail.ru>
2025-10-10 14:15:02 +0300

    second commit, fixed error

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -97,7 +97,6 @@ $.prototype.closest = function(selector) {
 
     for (let i = 0; i < this.length; i++) {
         let this1 = this[i].closest(selector);
-        debugger
         if(!this1) { //мое условие
             // this[i] = 'classNull';
             return this;
@@ -110,7 +109,6 @@ $.prototype.closest = function(selector) {
     for (; counter < objLength; counter++) {
         delete this[counter];
     }
-    debugger
     return this;
 };
 
